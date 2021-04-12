8d8d568059e494d7a1a64e3dd0a6642fa9320af1
Nixon <nixon613@gmail.com>
2021-04-12 08:57:46 +0600

    added main.go

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// take input from cli
+	// break those inputs to list of strings
+	// support parallel keywords in cli
+	// print those urls
+	// make the md5 hash of the request > maybe redirected request
+	// add unit tests
+}
